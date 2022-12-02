7752bef5288e8b4d0a3790b778d543600a263ce8
0xIryna <iryna@layerzerolabs.org>
2022-12-02 11:42:02 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# mainnet-testnet-bridge
+<div align="center">
+    <img alt="LayerZero" src="resources/LayerZeroLogo.png"/>
+</div>
+
+---
+
+# Mainnet to Testnet Bridge
+
+Mainnet to Testnet Bridge creates a market on both testnets and mainnets for native gas tokens.
+
+## Getting Started
+
+### Setup
+
+- Clone the repository
+- run `yarn`
+
+### Test
+
+`yarn test`
+
+Run the full suite of unit tests.
+
+### Coverage
+
+`yarn coverage`
+
+Get the coverage report.
---
+++ hardhat.config.js
@@ -0,0 +1,148 @@
+require("dotenv").config();
+
+require('hardhat-contract-sizer');
+require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+require('hardhat-gas-reporter');
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
+require('@openzeppelin/hardhat-upgrades');
+require('./tasks');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+function getMnemonic(networkName) {
+  if (networkName) {
+    const mnemonic = process.env['MNEMONIC_' + networkName.toUpperCase()]
+    if (mnemonic && mnemonic !== '') {
+      return mnemonic
+    }
+  }
+
+  const mnemonic = process.env.MNEMONIC
+  if (!mnemonic || mnemonic === '') {
+    return 'test test test test test test test test test test test junk'
+  }
+
+  return mnemonic
+}
+
+function accounts(chainKey) {
+  return { mnemonic: getMnemonic(chainKey) }
+}
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1
+          }
+        }
+      }    
+    ]
+  },
+  contractSizer: {
+    alphaSort: false,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+
+  namedAccounts: {
+    deployer: {
+      default: 0,    // wallet address 0, of the mnemonic in .env
+    }
+  },
+
+  networks: {
+    ethereum: {
+      url: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      chainId: 1,
+      accounts: accounts(),
+    },
+    bsc: {
+      url: "https://bsc-dataseed1.binance.org",
+      chainId: 56,
+      accounts: accounts(),
+    },
+    avalanche: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      chainId: 43114,
+      accounts: accounts(),
+    },
+    polygon: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      chainId: 137,
+      accounts: accounts(),
+    },
+    arbitrum: {
+      url: `https://arb1.arbitrum.io/rpc`,
+      chainId: 42161,
+      accounts: accounts(),
+    },
+    optimism: {
+      url: `https://mainnet.optimism.io`,
+      chainId: 10,
+      accounts: accounts(),
+    },
+    fantom: {
+      url: `https://rpcapi.fantom.network`,
+      chainId: 250,
+      accounts: accounts(),
+    },
+
+    goerli: {
+      url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      chainId: 5,
+      accounts: accounts(),
+    },
+    'bsc-testnet': {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      chainId: 97,
+      accounts: accounts(),
+    },
+    fuji: {
+      url: `https://api.avax-test.network/ext/bc/C/rpc`,
+      chainId: 43113,
+      accounts: accounts(),
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      chainId: 80001,
+      accounts: accounts(),
+    },
+    'arbitrum-goerli': {
+      url: `https://goerli-rollup.arbitrum.io/rpc/`,
+      chainId: 421613,
+      accounts: accounts(),
+    },
+    'optimism-goerli': {
+      url: `https://goerli.optimism.io/`,
+      chainId: 420,
+      accounts: accounts(),
+    },
+    'fantom-testnet': {
+      url: `https://rpc.testnet.fantom.network/`,
+      chainId: 4002,
+      accounts: accounts(),
+    }
+  }
+};
