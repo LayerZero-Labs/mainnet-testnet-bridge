fde730f6a0d4902fec211c14a7280bf00da8a7ce
0xIryna <iryna@layerzerolabs.org>
2022-12-04 14:49:00 -0800

    Unit testing

--- contracts/mocks/LayerZeroEndpoint.sol
+++ contracts/mocks/LayerZeroEndpoint.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.10;
 
 import "@layerzerolabs/solidity-examples/contracts/mocks/LZEndpointMock.sol";
+import "@layerzerolabs/solidity-examples/contracts/token/oft/OFT.sol";
 
 contract LayerZeroEndpoint is LZEndpointMock {
 	constructor(uint16 _chainId) LZEndpointMock(_chainId) {}
--- test/SwapBridge.test.js
+++ test/SwapBridge.test.js
@@ -1,13 +1,13 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
-const { utils } = require("ethers");
+const { utils, constants } = require("ethers");
 const { createUniswap } = require("./helpers");
 
 describe("SwapBridge", function () {
 	const mainnetId = 101
-	const testnetId = 10121
+	const goerliEthId = 10121
 
-	let owner, uniswap, testnetNativeOFT, weth, swapBridge
+	let owner, uniswap, goerliEthNativeOFT, goerliEthOFT, weth, swapBridge
 
 	before(async () => {
 		[owner] = await ethers.getSigners();
@@ -19,13 +19,46 @@ describe("SwapBridge", function () {
 	beforeEach(async () => {
 		const endpointFactory = await ethers.getContractFactory("LayerZeroEndpoint")
 		const mainnetEndpoint = await endpointFactory.deploy(mainnetId);
-		const nativeOFTFactory = await ethers.getContractFactory("NativeOFT");
-		testnetNativeOFT = await nativeOFTFactory.deploy("Goerli ETH", "ETH", mainnetEndpoint.address);
+		const goerliEndpoint = await endpointFactory.deploy(goerliEthId);		
+		
+		const goerliEthOFTFactory = await ethers.getContractFactory("OFT");
+		goerliEthOFT = await goerliEthOFTFactory.deploy("Goerli ETH OFT", "ETH", mainnetEndpoint.address);
+
+		const goerliEthNativeOFTFactory = await ethers.getContractFactory("NativeOFT");
+		goerliEthNativeOFT = await goerliEthNativeOFTFactory.deploy("Goerli ETH Native OFT", "ETH", goerliEndpoint.address);
+
 		const swapBridgeFactory = await ethers.getContractFactory("SwapBridge");
-		swapBridge = await swapBridgeFactory.deploy(testnetNativeOFT.address, uniswap.router.address, testnetId);
+		swapBridge = await swapBridgeFactory.deploy(goerliEthOFT.address, uniswap.router.address, goerliEthId);
+
+		await goerliEthOFT.setTrustedRemoteAddress(goerliEthId, goerliEthNativeOFT.address);
+		await goerliEthNativeOFT.setTrustedRemoteAddress(mainnetId, goerliEthOFT.address);		
 	})
 
-	it("swaps and bridges to testnet", async() => {
+	describe("bridges native Goerli ETH to mainnet", function () {
+		beforeEach(async () => {
 
-	})
+		})
+
+		it("Goerli ETH OFT is on mainnet", async () => {
+
+		})
+
+		describe("creates ETH - ETH Goerli OFT pool on mainnet", function () {
+			beforeEach(async () => {
+				const goerliEthAmount = utils.parseEther("20");
+				const ethAmount = utils.parseEther("10");
+				await goerliEthOFT.deposit({ value: goerliEthAmount });
+				await goerliEthOFT.approve(uniswap.router.address, goerliEthAmount);
+				await uniswap.router.addLiquidityETH(goerliEthOFT.address, goerliEthAmount, goerliEthAmount, ethAmount, owner.address, 2e9, { value: ethAmount });
+			})
+
+			it("swaps and bridges to testnet", async () => {
+				// const amountIn = utils.parseEther("0.1");
+				// const amounts = await uniswap.router.getAmountsOut(amountIn, [weth.address, goerliEthOFT.address]);
+				// const amountOutMin = amounts[1];
+				// await goerliEthOFT.approve(swapBridge.address, amountOutMin);
+				// await swapBridge.swapAndBridge(amountIn, amountOutMin, owner.address, constants.AddressZero, "0x", { value: utils.parseEther("0.2") });
+			})
+		})
+	})	
 })
