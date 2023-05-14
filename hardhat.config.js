9e818e1ec2d08cbe64a737374e150a7bc2c48d49
codingsh <codingsh@pm.me>
2023-05-14 23:27:05 +0100

    start development

--- hardhat.config.js
+++ hardhat.config.js
@@ -88,10 +88,14 @@ module.exports = {
       chainId: 10,
       accounts: accounts(),
     },
-
     "goerli-mainnet": {
       url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
       chainId: 5,
+      accounts: accounts(),
+    },
+    fuse: {
+      url: "https://rpc.fuse.io/",
+      chainId: 122,
       accounts: accounts(),
     }
   }
