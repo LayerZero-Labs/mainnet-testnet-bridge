0505e2fec71c5901abd7a87173104610bf8b6fba
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2024-03-01 17:58:15 -0800

    Sepolia Bridge

---
+++ contracts/SwapAndBridgeUniswapV3.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@uniswap/universal-router/contracts/interfaces/IUniversalRouter.sol";
+import "@layerzerolabs/solidity-examples/contracts/token/oft/IOFTCore.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SwapAndBridgeUniswapV3 {
+    uint24 public constant poolFee = 3000; // 0.3%
+
+    IUniversalRouter public immutable universalRouter;
+    address public immutable weth;
+	IOFTCore public immutable oft;
+
+    constructor(address _weth, address _oft, address _universalRouter) {
+        require(_weth != address(0), "SwappableBridge: invalid WETH address");
+        require(_oft != address(0), "SwappableBridge: invalid OFT address");
+        require(_universalRouter != address(0), "SwappableBridge: invalid Universal Router address");
+
+        weth = _weth;
+        oft = IOFTCore(_oft);
+        universalRouter = IUniversalRouter(_universalRouter);
+    }
+
+    function swapAndBridge(uint amountIn, uint amountOutMin, uint16 dstChainId, address to, address payable refundAddress, address zroPaymentAddress, bytes calldata adapterParams) external payable {
+        require(to != address(0), "SwappableBridge: invalid to address");
+        require(msg.value >= amountIn, "SwappableBridge: not enough value sent");
+
+        // 1) commands
+        // 0x0b == WRAP_ETH ... https://docs.uniswap.org/contracts/universal-router/technical-reference#wrap_eth
+        // 0x00 == V3_SWAP_EXACT_IN... https://docs.uniswap.org/contracts/universal-router/technical-reference#v3_swap_exact_in
+        bytes memory commands = hex"0b00";
+
+        // 2) inputs
+        bytes[] memory inputs = new bytes[](2);
+        // For weth deposit
+        inputs[0] = abi.encode(
+            address(universalRouter), // recipient
+            amountIn // amount
+        );
+        // For token swap
+        inputs[1] = abi.encode(
+            address(this), // recipient
+            amountIn, // amount input
+            amountOutMin, // min amount output
+            // pathway(inputToken, poolFee, outputToken)
+            abi.encodePacked(weth, poolFee, address(oft)),
+            false
+        );
+
+        // 3) deadline
+        uint256 deadline = block.timestamp;
+
+        // Call execute on the universal Router
+        universalRouter.execute{value: amountIn}(commands, inputs, deadline);
+
+        // check balance after to see how many tokens we received
+        uint256 amountReceived = IERC20(address(oft)).balanceOf(address(this));
+        // redundant check to ensure we received enough tokens
+        require(amountReceived >= amountOutMin, "SwappableBridge: insufficient amount received");
+
+        oft.sendFrom{value: msg.value - amountIn}(
+            address(this),
+            dstChainId,
+            abi.encodePacked(to),
+            amountReceived,
+            refundAddress,
+            zroPaymentAddress,
+            adapterParams
+        );
+    }
+}
--- deploy/SwapAndBridgeUniswapV3.js
+++ deploy/SwapAndBridgeUniswapV3.js
@@ -2,14 +2,14 @@ const WETHS = require("../constants/weths.json")
 const OFT = {
 	"ethereum": "0xE71bDfE1Df69284f00EE185cf0d95d0c7680c0d4",
 	"sepolia-mainnet": "0x4f7A67464B5976d7547c860109e4432d50AfB38e",
-}
-const NATIVE_OFT = {
-	"ethereum": "0x4f7A67464B5976d7547c860109e4432d50AfB38e",
-	"sepolia-mainnet": "0x2e5221B0f855Be4ea5Cefffb8311EED0563B6e87",
+	"arbitrum": "0xE71bDfE1Df69284f00EE185cf0d95d0c7680c0d4",
+	"optimism": "0xE71bDfE1Df69284f00EE185cf0d95d0c7680c0d4",
 }
 const UNIVERSAL_ROUTER = {
 	"ethereum": "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
 	"sepolia-mainnet": "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
+	"arbitrum": "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
+	"optimism": "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
 }
 
 module.exports = async function ({ deployments, getNamedAccounts }) {
@@ -23,18 +23,15 @@ module.exports = async function ({ deployments, getNamedAccounts }) {
 	const oft = OFT[hre.network.name]
 	console.log(`[${hre.network.name}] OFT Address: ${oft}`)
 
-	const nativeOft = NATIVE_OFT[hre.network.name]
-	console.log(`[${hre.network.name}] Native OFT Address: ${nativeOft}`)
-
 	const universalRouter = UNIVERSAL_ROUTER[hre.network.name]
 	console.log(`[${hre.network.name}] Universal Router Address: ${universalRouter}`)
 
-	await deploy("SwappableBridgeUniswapV3", {
+	await deploy("SwapAndBridgeUniswapV3", {
 		from: deployer,
-		args: [wethAddress, oft, nativeOft, universalRouter],
+		args: [wethAddress, oft, universalRouter],
 		log: true,
 		waitConfirmations: 1,
 	})
 }
 
-module.exports.tags = ["SwappableBridgeUniswapV3"]
+module.exports.tags = ["SwapAndBridgeUniswapV3"]
