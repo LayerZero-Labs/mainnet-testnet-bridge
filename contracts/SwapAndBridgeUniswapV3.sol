9c80a2c5bfaa64bee5f98c7cd450010f8eecca19
lz.sirΔrthurmoney() <95722332+sirarthurmoney@users.noreply.github.com>
2024-03-01 18:05:08 -0800

    Sepolia Bridge (#34)
    * Initial setup for bridge and swap UniSwap Universal Router
    
    * Further optimizations
    
    * Remove unused dependencies
    
    * Sepolia Bridge
    
    ---------
    
    Co-authored-by: lz.wΔrlock() <trileyschwarz@gmail.com>

--- contracts/SwapAndBridgeUniswapV3.sol
+++ contracts/SwapAndBridgeUniswapV3.sol
@@ -4,25 +4,21 @@ pragma solidity ^0.8.0;
 import "@uniswap/universal-router/contracts/interfaces/IUniversalRouter.sol";
 import "@layerzerolabs/solidity-examples/contracts/token/oft/IOFTCore.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./INativeOFT.sol";
 
-contract SwappableBridgeUniswapV3 {    
+contract SwapAndBridgeUniswapV3 {
     uint24 public constant poolFee = 3000; // 0.3%
 
-    INativeOFT public immutable nativeOft;
     IUniversalRouter public immutable universalRouter;
     address public immutable weth;
 	IOFTCore public immutable oft;
 
-    constructor(address _weth, address _oft, address _nativeOft, address _universalRouter) {
+    constructor(address _weth, address _oft, address _universalRouter) {
         require(_weth != address(0), "SwappableBridge: invalid WETH address");
         require(_oft != address(0), "SwappableBridge: invalid OFT address");
-        require(_nativeOft != address(0), "SwappableBridge: invalid Native OFT address");
         require(_universalRouter != address(0), "SwappableBridge: invalid Universal Router address");
 
         weth = _weth;
         oft = IOFTCore(_oft);
-        nativeOft = INativeOFT(_nativeOft);
         universalRouter = IUniversalRouter(_universalRouter);
     }
 
@@ -72,13 +68,5 @@ contract SwappableBridgeUniswapV3 {
             zroPaymentAddress,
             adapterParams
         );
-    }
-
-    function bridge(uint amountIn, uint16 dstChainId, address to, address payable refundAddress, address zroPaymentAddress, bytes calldata adapterParams) external payable {
-        require(to != address(0), "SwappableBridge: invalid to address");
-        require(msg.value >= amountIn, "SwappableBridge: not enough value sent");
-
-        nativeOft.deposit{value: amountIn}();
-        nativeOft.sendFrom{value: msg.value - amountIn}(address(this), dstChainId, abi.encodePacked(to), amountIn, refundAddress, zroPaymentAddress, adapterParams);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -93,6 +93,11 @@ module.exports = {
       url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
       chainId: 5,
       accounts: accounts(),
+    },
+    "sepolia-mainnet": {
+      url: "https://rpc.sepolia.ethpandaops.io",
+      chainId: 11155111,
+      accounts: accounts(),
     }
   }
 };
